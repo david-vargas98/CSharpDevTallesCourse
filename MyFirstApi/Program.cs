4b03a839831a45d1a2d92c0c35290b72c10f5d76
david-vargas98 <edgar981125@live.com.mx>
2025-10-08 11:46:15 -0600

    feat: adding gitignore, models and controllers folder and programs.cs cleaning.

--- MyFirstApi/Program.cs
+++ MyFirstApi/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -14,28 +15,22 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// adding middleware to include static files such as index.html
+app.UseStaticFiles();
 
-app.MapGet("/weatherforecast", () =>
+// redirection from root path to index.html
+app.Use(async (context, next) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/Index.html");
+    }
+    else
+    {
+        await next();
+    }
+});
+
+app.MapControllers(); // add controller endpoints
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
